408bb859916a694b6bc2eb5e979385b68c87529b
Sabino Neria Cervantes <n_eria_11@hotmail.com>
2015-11-25 15:45:25 -0600

    Cambios
    Sesion destroy

--- models/security/control_sesion.php
+++ models/security/control_sesion.php
@@ -7,6 +7,7 @@ $tiempo_transcurrido = $segundos;
 $tiempo_maximo = $_SESSION["inicio"]  + ( $_SESSION["intervalo"] * 60 ) ; // se multiplica por 60 segundos ya que se configura en minutos
 if($tiempo_transcurrido > $tiempo_maximo){
 header("Location: index.php");
+session_destroy();
 }else{
 // se resetea el inicio
 $_SESSION["inicio"] = time();
